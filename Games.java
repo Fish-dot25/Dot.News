fc8d3a36f7184eade95d72c265cce66e4fb7f373
Fish-dot25 <kinnu.parab@gmail.com>
2025-07-31 09:26:01 -0400

    Update Games.java

--- Games.java
+++ Games.java
@@ -1,5 +1,11 @@
-background("lightblue");
-fill("green");
-ellipse(200, 500, 500, 400);
-fill("yellow");
-ellipse(0, 0, 200, 200);
+var data = {};
+onEvent("ClassBookRecommender_predict", "click", function() {
+    addPair(data, "Whichanimaldoyoulikethemost?", getText("animal_dropdown"));
+		addPair(data, "describe", getText("Describeyourself?_dropdown"));
+		addPair(data, "wherespendtime", getText("Wheredoyouliketospendyoutime?_dropdown"));
+		addPair(data, "howspendtime", getText("Howdoyoulikespendingyoutime?_dropdown"));
+    setText("ClassBookRecommender_prediction", '');
+    getPrediction("Class Book Recommender", "OV8kuQzp3Yuh", data, function(value) {
+      setText("ClassBookRecommender_prediction", value);
+    });
+  });
